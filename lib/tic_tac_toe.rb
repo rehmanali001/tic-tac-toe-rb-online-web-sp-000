f897a05c6788427564fdf1df62a34b208eb491f8
Rehman Ali <rehmanali001@gmail.com>
2019-07-10 00:05:46 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,7 +21,7 @@ def input_to_index(input)
   return input.to_i - 1
 end
 
-def move(board, index, current_player = "X")
+def move(board, index, current_player)
   board[index] = current_player
 end
 
