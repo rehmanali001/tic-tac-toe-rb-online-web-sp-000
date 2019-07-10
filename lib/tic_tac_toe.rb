4f41784568f3a6e7d95a2b83d4b007b0e8820791
Rehman Ali <rehmanali001@gmail.com>
2019-07-10 00:00:45 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -25,3 +25,10 @@ def move(array, index, value="X")
   array[index] = value
 end
 
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  end
+end
