4cdd3fb3bc665e1d84203964d8bb50f03efb18b8
Rehman Ali <rehmanali001@gmail.com>
2019-07-10 00:02:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -41,6 +41,18 @@ def valid_move?(board, index)
   end
 end
 
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
 
 
 
